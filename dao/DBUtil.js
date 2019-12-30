2efdce4c06c2f946f6e1d07c89b866871bbd1021
Emmie-github <aixiao92@foxmail.com>
2019-12-30 09:35:04 +0800

    update host

--- dao/DBUtil.js
+++ dao/DBUtil.js
@@ -2,7 +2,7 @@
 const mysql = require("mysql");
 function createConnection() {
   const connection = mysql.createConnection({
-    host: "192.168.0.6",
+    host: "192.168.0.3",
     port: "3306",
     user: "root",
     password: "123456",
